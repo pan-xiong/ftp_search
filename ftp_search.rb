b53f5b927355c3abe02b57b98302281b03cf1fc1
rita <rita@agideo.com>
2014-09-11 15:43:03 +0800

    fix find

--- ftp_search.rb
+++ ftp_search.rb
@@ -1,6 +1,7 @@
 require 'net/ftp'
 require 'date'
 require 'optparse'
+require 'io/console'
 
 class FtpSearch
   def option_parser
@@ -21,7 +22,7 @@ class FtpSearch
         options[:username] = value
       end
 
-      opts.on('-p PASSWORD', '--password PASSWORD', 'ftp password') do |value|
+      opts.on('-p [PASSWORD]', '--password [PASSWORD]', 'ftp password') do |value|
         options[:password] = value
       end
 
@@ -37,18 +38,35 @@ class FtpSearch
     @server = options[:host]
     @user = options[:username]
     @password = options[:password]
-    @path = options[:path]
+    @path = options[:path] || '/'
     @keywords = options[:match]
+    if @password.nil?
+      print "Enter password: "
+      @password = STDIN.noecho(&:gets).chomp
+      puts ""
+    end
+    puts "loading...."
   end
 
   def do_search
     option_parser
+    find(nil, @path)
+  end
+
+  def find(ftp, path)
     Net::FTP.open(@server, @user, @password) do |ftp|
-      ftp.chdir(@path) unless @path.nil?
-      if @keywords.nil? || @keywords.empty?
-        p ftp.nlst
-      else
-        p ftp.nlst.select{|i| i.to_s.scan(@keywords).any?}
+      ftp.chdir(path) unless path.nil?
+      ftp.ls.each do |i|
+        short_name = i.split(' ')[-1]
+        full_name = "#{path}/#{short_name}"
+        full_name += '/' if i[0].downcase == 'd'
+        puts full_name if short_name.to_s.scan(@keywords).any?
+      end
+      dirs = ftp.ls.select{|i|i[0]=='d'}.map{|j| j.split(' ')[-1]}
+      return if dirs.empty?
+      dirs.each do |dir_name|
+        full_dir = "#{path == '/' ? '' : path}/#{dir_name}"
+        find(nil, full_dir)
       end
     end
   end
